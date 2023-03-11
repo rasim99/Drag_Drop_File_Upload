700d2cc5719bd161e7af38d0be5488894f999efc
rasim99 <rasimjm@code.edu.az>
2023-03-11 12:37:33 +0400

    drag_drop File Upload + x +REmoveAll

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,8 @@ let UploadIcon=document.getElementById("upload");
 let Table=document.querySelector(".table");
 let Remove=document.getElementById("remove");
 let AreaDrop=document.getElementById("areadrop");
-
+let RemoveTH=document.querySelectorAll(".thtable");
+let CheckTH;
 let count=0;
 AreaDrop.ondragover=function (e) {
    e.preventDefault();
@@ -18,15 +19,18 @@ UploadIcon.addEventListener("click",function () {
 })
 
 UploadIcon.nextElementSibling.onchange=function(e) {
-  //////////////////////////////////
-   Table.classList.remove("d-none")
-   /////////////////////////////////
-
-   /////////////////////////////////
+    CheckTH?CheckTH.classList.remove("d-none"):"";
+   Table.classList.remove("d-none");
    Remove.classList.remove("d-none");
-   /////////////////////////////////
             Upl(e.target.files)
+           for (let i = 1; i < RemoveTH.length; i++) {
+           CheckTH=RemoveTH[i];
+            
+           }
+   // RemoveTH.classList.remove("d-none")
+
 }
+
 
 function Upl(files) {
    Array.from(files).forEach(file=>{
@@ -35,12 +39,11 @@ function Upl(files) {
       
       reader.onloadend=function (e) {
          let tr=document.createElement("tr");
-         
-         //////////////////////////////////////////
+          
          let tdCount=document.createElement("td");
          count++;
          tdCount.innerText+=count;
-        /////////////////////////////////////////////
+        
  
          let tdName=document.createElement("td");
           tdName.innerText=file.name;
@@ -54,24 +57,27 @@ function Upl(files) {
         
          let tdSize=document.createElement("td");
          tdSize.innerText=file.size;
-        ///////////////////////////////////////////////
+        
          let tdRemove=document.createElement("td");
          tdRemove.innerText="X";
-         /////////////////////////////////////////////
+         tdRemove.style.cursor="pointer";
+         tdRemove.addEventListener("click",function () {
+            tr.remove();
+         })
+         
  
          tr.append(tdCount,tdName,tdImage,tdSize,tdRemove);
          Table.lastElementChild.append(tr);
       }
       reader.readAsDataURL(file);
-     
-    
     })
 }
 
 
-   Remove.onclick=function() {          
-     Remove.classList.add("d-none");
-    Table.classList.add("d-none"); 
-    // Remove.remove();
-    // Table.remove() ;
+  Remove.onclick=function() {          
+      Remove.classList.add("d-none");
+       RemoveTH.forEach(th=>{
+        th.classList.add("d-none")
+       })
+         Table.lastElementChild.remove(); 
    }
